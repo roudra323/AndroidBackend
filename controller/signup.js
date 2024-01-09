b18a4204bcb757d13a9ebeb3f02cc55a9f59b03a
roudra323 <asirroudra@gmail.com>
2024-01-10 04:04:43 +0600

    added dob

--- controller/signup.js
+++ controller/signup.js
@@ -5,67 +5,65 @@ const sendEmail = require("./email");
 const crypto = require("crypto");
 const Token = require("../model/token");
 
-
 const signupData = async (req, res) => {
-    try {
-        const { email } = req.body;
-        // console.log(req.body);
-        let user = await User.findOne({ email });
-        if (user) {
-            return res.status(400).json({
-                message: "User email already exists!",
-            });
-        }
-
-        user = new User(req.body);
-        await user.save();
-
-        let token = await new Token({
-            userId: user._id,
-            token: crypto.randomBytes(32).toString("hex"),
-        }).save();
-
-        const message = `${process.env.BASE_URL}/auth/verify/${user._id}/${token.token}`;
-        await sendEmail(user.name,user.email, "Verify Email", message);
-        console.log(message);
-
-        res.send("An Email sent to your account please verify");
-
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({
-            error: "There was a server-side error!",
-        });
+  try {
+    const { email } = req.body;
+    console.log(req.body);
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        message: "User email already exists!",
+      });
     }
+
+    user = new User(req.body);
+    await user.save();
+
+    let token = await new Token({
+      userId: user._id,
+      token: crypto.randomBytes(32).toString("hex"),
+    }).save();
+
+    const message = `${process.env.BASE_URL}/auth/verify/${user._id}/${token.token}`;
+    await sendEmail(user.name, user.email, "Verify Email", message);
+    console.log(message);
+
+    res.send("An Email sent to your account please verify");
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      error: "There was a server-side error!",
+    });
+  }
 };
 
 const verifyEmail = async (req, res) => {
-    try {
-        const { userId, token } = req.params;
-        const user = await User.findById(userId);
-        if (!user) {
-            return res.status(400).json({
-                message: "User not found!",
-            });
-        }
-
-        const tokenDoc = await Token.findOneAndDelete({ userId, token });
-        if (!tokenDoc) {
-            return res.status(400).json({
-                message: "Invalid or expired link!",
-            });
-        }
-
-        user.verified = true;
-        await user.save();
-
-        res.send("Your account has been successfully verified!");
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({
-            error: "There was a server side error!",
-        });
+  try {
+    const { userId, token } = req.params;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(400).json({
+        message: "User not found!",
+      });
     }
+
+    const tokenDoc = await Token.findOneAndDelete({ userId, token });
+    if (!tokenDoc) {
+      return res.status(400).json({
+        message: "Invalid or expired link!",
+      });
+    }
+
+    user.verified = true;
+    await user.save();
+
+    res.send("Your account has been successfully verified!");
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      error: "There was a server side error!",
+    });
+  }
 };
 
 module.exports = { signupData, verifyEmail };
--- model/signupSchema.js
+++ model/signupSchema.js
@@ -1,67 +1,70 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt"); 
+const bcrypt = require("bcrypt");
 
 const signupSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    phone: {
-        type: String,
-        min: [11, "Phone number must be at least 11 digits"],
-    },
-    presentAddress: {
-        type: String,
-        required: true,
-    },
-    dept: {
-        type: String,
-        required: true,
-    },
-    batch: {
-        type: String,
-        required: true,
-    },
-    bloodGroup: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: [6, "Password must be at least 6 characters"],
-        maxlength: [255, "Password cannot exceed 255 characters"],
-    },
-    profilePicture: {
-        type: String,
-        default:"demo.jpg",
-    },
-    date: {
-        type: Date,
-        default: Date.now,
-    },
-    verified: {
-        type: Boolean,
-        default: false,
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    min: [11, "Phone number must be at least 11 digits"],
+  },
+  presentAddress: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: String,
+    required: true,
+  },
+  dept: {
+    type: String,
+    required: true,
+  },
+  batch: {
+    type: String,
+    required: true,
+  },
+  bloodGroup: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: [6, "Password must be at least 6 characters"],
+    maxlength: [255, "Password cannot exceed 255 characters"],
+  },
+  profilePicture: {
+    type: String,
+    default: "demo.jpg",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 signupSchema.pre("save", async function (next) {
-    try {
-        if (!this.isModified("password")) {
-            return next();
-        }
-        const hashedPassword = await bcrypt.hash(this.password, 10);
-        this.password = hashedPassword;
-        next();
-    } catch (error) {
-        return next(error);
+  try {
+    if (!this.isModified("password")) {
+      return next();
     }
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    return next(error);
+  }
 });
-
 
 module.exports = signupSchema;
